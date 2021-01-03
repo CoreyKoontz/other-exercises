d6f3fa9796310110775914f9a5e54022af251138
Corey Koontz <corey.michael.koontz@gmail.com>
2021-01-02 18:42:24 -0600

    feat: Adding a few for solutions

--- functionDrills.js
+++ functionDrills.js
@@ -32,4 +32,44 @@ function isMultipleOfThree(input) {
     return input % 3 === 0;
 }
 
-function
+function isMultipleOfThreeAndFive(input) {
+    return input % 3 === 0 && input % 5 === 0
+}
+function isMultipleOf(target, n) { //Asking if 'target' is is a multiple of 'n'
+    return target % n === 0;
+}
+
+function isTrue(input) {
+    return input === true;
+}
+
+function isFalse(input) {
+    return input === false;
+}
+
+/*All values are truthy unless they are defined as falsy
+(i.e., except for false, 0, -0, 0n, "", null, undefined, and NaN
+ */
+function isTruthy(input) {
+    return input == true;
+}
+function isFalsy(input) {
+    return input == !true; //doesnt work, keeps returning NaN as false
+}
+
+function isVowel(x) {
+    return x == "a" ||x == "e"||x == "i"||x == "o"||x == "u"||x == "A"||x == "E"||x == "I"||x == "O"||x == "U"
+}
+
+function isConsonant(input) {
+    if (input == (['a'||'e'||'i'||'o'||'u'||'A'||'E'||'I'||'O'||'U']) || (typeof input != "string")) {
+        return false;
+    } else {
+        return true;
+    }
+} // figure out how we can return false for a number entered as a string ("5" is returning true)
+
+function isCapitolLetter(input) {
+  return input === input.toUpperCase();
+} // figure out how we can return false for a number entered as a string ("5" is returning true)
+
