06964f7d088a6005a434087d5be8913205665187
Corey Koontz <corey.michael.koontz@gmail.com>
2021-01-15 12:23:45 -0600

    Adding files for 101 Exercise

---
+++ js/101-exercises.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//############### 101-Exercises ###############//
+//#############################################//
+
+
+// 57.
+// Write a function definition named firstAndSecond that takes in sequence and returns the first and second value of that sequence as an array.
+
+function firstAndSecond () {
+
+}
