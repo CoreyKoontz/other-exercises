6b6438ac4a6dd240cfe876fc86bb46d9d61dd09b
Corey Koontz <corey.michael.koontz@gmail.com>
2021-01-17 19:09:10 -0600

    Restart 101 Exercises for a refresh

--- js/101-exercises.js
+++ js/101-exercises.js
@@ -3,10 +3,142 @@
 //############### 101-Exercises ###############//
 //#############################################//
 
+// 1.
+var onMarsRightNow = false;
+
+// 2.
+var fruits = ["mango", "banana", "guava", "kiwi", "strawberry"];
+
+// 3.
+var vegetables = ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini"];
+
+// 4.
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// 5.
+fruits.push("tomato");
+
+// 6.
+vegetables.push("tomato");
+
+// 7.
+var someNumbers = numbers.reverse();
+
+// 8.
+vegetables.sort();
+
+// 9.
+fruits.sort().reverse();
+
+// 10.
+var fruitsAndVeggies = fruits.concat(vegetables);
+
+// 11.
+function addOne (x) {
+   return parseFloat(x) + 1;
+}
+
+// 12.
+function isPositive (x) {
+    return parseFloat(x) > 0;
+}
+
+// 13.
+function isNegative (x) {
+    return parseFloat(x) < 0;
+}
+
+// 14.
+function isOdd(x) {
+    return parseFloat(x) % 2 !== 0
+}
+
+// 15.
+function isEven (x) {
+    return parseFloat(x) % 2 == 0;
+}
+// 16.
+function identity(x) {
+    return x;
+}
+// 17.
+function isPositiveOdd(x) {
+    var x = parseFloat(x);
+    return (x % 2 !== 0) && (x > 0);
+}
+// 18.
+
+
+// 19.
+
+
+// 20.
+
+
+// 21.
+
+
+// 22.
+
+
+// 23.
+
+
+// 24.
+
+
+// 25.
+
+
+// 26.
+
+
+// 27.
+
+
+// 28.
+
+
+// 29.
+
+
+// 30.
+
+
+// 31.
+
+
+// 32.
+
+
+// 33.
+
+
+// 34.
+
+
+// 35.
+
+
+// 36.
+
+
+// 37.
+
+
+// 38.
+
+
+// 39.
+
+
+// 40.
+
+
 
 // 57.
 // Write a function definition named firstAndSecond that takes in sequence and returns the first and second value of that sequence as an array.
 
-function firstAndSecond () {
+function firstAndSecond (a, b, c) {
 
 }
